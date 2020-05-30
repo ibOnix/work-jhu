30e10a90dc79e2d4c505b20cb273371aa22ae504
ibOnix <webmaster@kilobyte.es>
2020-05-30 17:05:07 +0200

    aaded function to guessinggame.sh and it's running. I'm so excited.

--- guessinggame.sh
+++ guessinggame.sh
@@ -9,13 +9,21 @@ echo "Nice to meet you $name. Would you like to play riddles with me? (y / n)"
 read response
 
 [[ $response =~ y ]] && echo "Great! Go for it! Do you know how many files are in the current directory?" || echo "Seriously? You kill me... and the script too! May be next time."
-read attempt
-if [[ $attempt -eq $answer ]]
-then
-	echo "It's magic! Or no file here? Anyway, congratulations."
-	echo "If you want to play again, just take me to another place in your computer."
 
-elif [[ $attempt -gt $answer ]] && echo "Your guess $attempt was too high." || echo "Your guess $attempt was too low."
-then
-	echo "Try again. The program will end when you hit the answer."
-fi
+function check {
+	read attempt
+	if [[ $attempt -eq $answer ]]
+	then
+		echo "It's magic! Or no file here? Anyway, congratulations."
+		echo "If you want to play again, just take me to another place in your computer."
+
+		elif [[ $attempt -gt $answer ]] && echo "Your guess $attempt was too high." || echo "Your guess $attempt was too low."
+		then
+		echo "Try again. The program will end when you hit the answer."
+		fi
+}
+
+while [[ $attempt -ne $answer ]]
+do
+	check
+done
--- makefile
+++ makefile
@@ -1,9 +1,13 @@
 ## Johns Hopkings University | The Unix Workbench | Guessing Game
 
+## Variables
+
+last-update = $(date -R -r README.md)
+lines = $(wc -l guessinggame.sh)
+
 README.md:
 	touch README.md
-	echo "## Johns Hopkings University | The Unix Workbench | Guessing Game." > README.md
-	echo "## The make file was run:" > README.md
-		$(date -R -r README.md)			
-	echo "## The guessinggame.sh file contains the following number of lines:" > README.md
-		wc -l guessinggame.sh
+		echo "## Johns Hopkings University | The Unix Workbench | Guessing Game." > README.md
+		echo "## The make file was run:  $last-update" > README.md
+		echo "## The guessinggame.sh file contains the following number of lines: $lines" > README.md
+		
